37e1c478e48a70e3088f34471e5bb1e45e35c4fb
Ricardo Riscanevo Cotrina <rriscanevoc@ccxc.co>
2025-03-06 12:22:15 -0500

    Codigo de prueba

---
+++ Test.py
@@ -0,0 +1,11 @@
+def es_primo(num, n=2):
+    if n >= num:
+        print("Es primo")
+        return True
+    elif num % n != 0:
+        return es_primo(num, n + 1)
+    else:
+        print("No es primo", n, "es divisor")
+        return False
+    
+es_primo(33)
